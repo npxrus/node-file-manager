bb4965bbb482e37376dbadcfd0eb7c5299c085f7
npxrus <npxrus@yandex.com>
2022-06-12 14:00:24 +0300

    feat: add os operations

--- file-manager.js
+++ file-manager.js
@@ -1,7 +1,33 @@
+const readline = require('readline');
+const osHelper = require('./helpers/os.helper.js');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'Enter your command> ',
+});
+
+const username = process.env.npm_config_username;
+
 const init = () => {
-  console.log(
-    `Welcome to the File Manager, ${process.env.npm_config_username}`
-  );
+  console.log(`Welcome to the File Manager, ${username}`);
+
+  rl.prompt();
+  rl.on('line', (line) => {
+    const [command, ...args] = line.trim().split(' ');
+    switch (command) {
+      case 'os':
+        osHelper(args[0]);
+        break;
+      case '.exit':
+        console.log(`Thank you for using File Manager, ${username}!`);
+        process.exit(0);
+    }
+    rl.prompt();
+  }).on('close', () => {
+    console.log(`\nThank you for using File Manager, ${username}!`);
+    process.exit(0);
+  });
 };
 
 init();
